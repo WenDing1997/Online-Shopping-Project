dc5104d1e9bf0d88ee54ab822aebea0fbb869a0c
Wenyu Ding <wd87@cornell.edu>
2020-11-27 17:17:29 +0800

    使用Mybatis自动生成器

--- src/main/java/com/miaoshaproject/App.java
+++ src/main/java/com/miaoshaproject/App.java
@@ -1,7 +1,12 @@
 package com.miaoshaproject;
 
+import com.miaoshaproject.dao.UserDOMapper;
+import com.miaoshaproject.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,18 +16,27 @@ import javax.swing.*;
  * Hello world!
  *
  */
-@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = {"com.miaoshaproject"})
 @RestController
+@MapperScan("com.miaoshaproject.dao")
 public class App {
 
-    @RequestMapping("/")
-    public String home() {
-        return "Hello World!";
-    }
+  @Autowired
+  private UserDOMapper userDOMapper;
 
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
-        SpringApplication.run(App.class, args);
+  @RequestMapping("/")
+  public String home() {
+    UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+    if (userDO == null) {
+      return "用户对象不存在";
+    } else {
+      return userDO.getName();
     }
+  }
+
+  public static void main( String[] args ) {
+    System.out.println( "Hello World!" );
+    SpringApplication.run(App.class, args);
+  }
 
 }
